5004669c1528d1372dc5c357c473036438b96cd4
Mztrem7 <mamacogodzilla7@gmail.com>
2023-06-23 16:44:23 -0300

    Adição da função de piscar - (INCOMLPETO)

--- script.js
+++ script.js
@@ -6,80 +6,113 @@ const amarelo = document.getElementById('amarelo');
 const azul = document.getElementById('azul');
 const jogarMaquina = document.getElementById('jogarMaquina');
 const verificar = document.getElementById('verificar');
-const corpo = document.getElementsByTagName("body")
 
-let vetjogador=[], vetmaq=[];
+let vetjogador = [],
+    vetmaq = [];
 
 
-vermelho.onclick = ()=>{
-    setTimeout(()=>{
-        vermelho.style.backgroundColor='#FF0000';
-     },500);
-     vetjogador.push(1);
-     console.log('Jogador ' + vetjogador);
+vermelho.onclick = () => {
+    setTimeout(() => {
+        vermelho.style.backgroundColor = '#FF0000';
+    }, 500);
+    vetjogador.push(1);
+    console.log('Jogador ' + vetjogador);
 }
-verde.onclick = ()=>{
-    
-    setTimeout(()=>{
-        verde.style.backgroundColor='#228B22'
-     },500);
-     vetjogador.push(2);
-     console.log('Jogador ' + vetjogador);
-}
+verde.onclick = () => {
 
-amarelo.onclick = ()=>{
-    
-    setTimeout(()=>{
-        amarelo.style.backgroundColor='#FFD700'
-     },500);
-     vetjogador.push(3);
-     console.log('Jogador ' + vetjogador);
+    setTimeout(() => {
+        verde.style.backgroundColor = '#228B22'
+    }, 500);
+    vetjogador.push(2);
+    console.log('Jogador ' + vetjogador);
 }
 
-azul.onclick = ()=>{
-    
-    setTimeout(()=>{
-        azul.style.backgroundColor='#0000FF'
-     },500);
-     vetjogador.push(4);
-     console.log('Jogador ' + vetjogador);
+amarelo.onclick = () => {
+
+    setTimeout(() => {
+        amarelo.style.backgroundColor = '#FFD700'
+    }, 500);
+    vetjogador.push(3);
+    console.log('Jogador ' + vetjogador);
 }
-function jogar(){
+
+azul.onclick = () => {
+
+    setTimeout(() => {
+        azul.style.backgroundColor = '#0000FF'
+    }, 500);
+    vetjogador.push(4);
+    console.log('Jogador ' + vetjogador);
+}
+
+function jogar() {
     vetmaq.push(Math.floor(Math.random() * 4 + 1));
-    console.log('Máquina '+ vetmaq);
+    console.log(vetmaq);
+    pisca()
 }
-function verifygame2(){
+
+function pisca() {
+    vetmaq.map((i) => {
+        if (i == 1) {
+            setTimeout(() => {
+                vermelho.style.backgroundColor = "red"
+            }, 350)
+            vermelho.style.filter = "brightness(200)"
+        }
+        if (i == 2) {
+            setTimeout(() => {
+                verde.style.backgroundColor = "green"
+            }, 350)
+            verde.style.filter = "brightness(200)"
+        }
+        if (i == 3) {
+            setTimeout(() => {
+                amarelo.style.backgroundColor = "yellow"
+            }, 350)
+            amarelo.style.filter = "brightness(200)"
+        }
+        if (i == 4) {
+            setTimeout(() => {
+                azul.style.backgroundColor = "blue"
+            }, 350)
+            azul.style.filter = "brightness(200)"
+        }
+    })
+}
+
+function verifygame2() {
     tamVet = vetmaq.length;
-    let i = 0, resp;
+    let i = 0,
+        resp;
     while (i < tamVet) {
         if (vetjogador[i] != vetmaq[i]) {
-           resp="perdeu";
-           vetmaq=[];
-           setTimeout(()=>{
-            document.body.style.backgroundColor = " #030351";
-            document.body.style.transition = "0.3s ease";
-        },250);
-        document.body.style.backgroundColor = "red";
+            resp = "perdeu";
+            vetmaq = [];
+            setTimeout(() => {
+                document.body.style.backgroundColor = " #030351";
+                document.body.style.transition = "0.3s ease";
+            }, 250);
+            document.body.style.backgroundColor = "red";
             break;
-        }else{
-            resp="acertou";
-            setTimeout(()=>{
+        } else {
+            resp = "acertou";
+            setTimeout(() => {
                 document.body.style.backgroundColor = " #030351";
                 document.body.style.transition = "0.3s ease";
 
-            },250);
+            }, 250);
             document.body.style.backgroundColor = "green";
         }
         i += 1;
-     }
-     console.log( resp);
-     vetjogador=[];
-     
+    }
+    console.log(resp);
+    vetjogador = [];
+
 }
-verificar.onclick=()=>{
+verificar.onclick = () => {
     verifygame2();
 };
-jogarMaquina.onclick = ()=>{
+jogarMaquina.onclick = () => {
     jogar();
-    vetjogador=[];
+    vetjogador = [];
 };
